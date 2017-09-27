7a956f94182644f77f973e4a50acff44d73fe906
Carla Brady <carlakaybrady@gmail.com>
2017-09-27 16:37:41 -0500

    removed extra body-parsers code lines in peeps.js

--- modules/routes/peeps.js
+++ modules/routes/peeps.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
 // 27017 is default mongo port
+
 mongoose.connect('localhost:/27017/meanie');
-var peepsSchema = new mongoose.Schema({ //check on peeps schema
+
+var peepsSchema = new mongoose.Schema({
     name: String,
     location: String
 });
+
 var peepsModel = mongoose.model('peepsModel', peepsSchema);
-router.use(bodyParser.urlencoded({
-    extended: true
-}));
-router.use(bodyParser.json());
+
 router.get('/', function (req, res) {
     // get and send back all the things
     peepsModel.find().then(function (data) {
         res.send(data);
     });
 });
+
 router.post('/', function (req, res) { //changed app to router
     console.log('req.body.name: ' + req.body.name);
     // retrieved the req.body
